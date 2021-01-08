93476310a5794f2a0301036481218f5e8bf07898
Biljana Zivkovic <77129646+biljanazivkovic@users.noreply.github.com>
2021-01-08 16:45:53 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+namespace ex23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int[] myNumbers = { 1, 2, 3 };
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.Message);
+                Console.WriteLine("Something went wrong.");
+            }
+            finally
+            {
+                Console.WriteLine("The 'try...catch' is finished.");
+            }
+        }
+    }
+}
